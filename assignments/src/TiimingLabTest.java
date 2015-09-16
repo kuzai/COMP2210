a693cb7235151db87b460477b606aaaef55a1562
Kuzai <walkerhwood@gmail.com>
2015-09-16 15:19:13 -0400

    text

---
+++ assignments/src/TiimingLabTest.java
@@ -0,0 +1,33 @@
+Time complexity for Key: 71
+
+This call to method TimingLab.timeTrial(1) took 0.005 seconds.
+This call to method TimingLab.timeTrial(2) took 0.016 seconds.
+R = 3.2152776089352475
+K = 1.6849433060582837
+This call to method TimingLab.timeTrial(4) took 0.029 seconds.
+R = 1.8238805893663677
+K = 0.8670112784235625
+This call to method TimingLab.timeTrial(8) took 0.151 seconds.
+R = 5.168305565116296
+K = 2.36969136884103
+This call to method TimingLab.timeTrial(16) took 0.643 seconds.
+R = 4.262689152858263
+K = 2.0917638538050514
+This call to method TimingLab.timeTrial(32) took 1.930 seconds.
+R = 3.0015140280821577
+K = 1.5856904106592193
+This call to method TimingLab.timeTrial(64) took 7.170 seconds.
+R = 3.715064408946952
+K = 1.8933872233245288
+This call to method TimingLab.timeTrial(128) took 28.857 seconds.
+R = 4.024922445606608
+K = 2.008960984862859
+This call to method TimingLab.timeTrial(256) took 116.136 seconds.
+R = 4.0245157750636364
+K = 2.0088152103230623
+This call to method TimingLab.timeTrial(512) took 464.013 seconds.
+R = 3.9954383526116293
+K = 1.9983537946290597
+This call to method TimingLab.timeTrial(1024) took 1857.752 seconds.
+R = 4.003663615573474
+K = 2.0013207652501643
